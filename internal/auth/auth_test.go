1f6e023ab7ab2046f2e9f542a2d2d9746efdedbf
Guaty <bjarocha911@gmail.com>
2025-02-28 13:20:51 -0600

    Add JWT creation and validation with tests

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,6 +2,9 @@ package auth
 
 import (
 	"testing"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 func TestCheckPasswordHash(t *testing.T) {
@@ -54,6 +57,59 @@ func TestCheckPasswordHash(t *testing.T) {
 			err := CheckPasswordHash(tt.password, tt.hash)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("CheckPasswordHash() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestJWT(t *testing.T) {
+	// Let's get some initial variables set
+	userID := uuid.New()
+	tokenSecret := "mySecretKey42"
+	validExpirationTime := 10 * time.Minute
+	invalidExpirationTime := -2 * time.Minute
+
+	tokenString1, _ := MakeJWT(userID, tokenSecret, validExpirationTime)
+	tokenString2, _ := MakeJWT(userID, tokenSecret, invalidExpirationTime)
+
+	tests := []struct {
+		name        string
+		id          uuid.UUID
+		tokenString string
+		tokenSecret string
+		wantErr     bool
+	}{
+		{
+			name:        "Valid Token",
+			id:          userID,
+			tokenString: tokenString1,
+			tokenSecret: tokenSecret,
+			wantErr:     false,
+		},
+		{
+			name:        "Expired Token",
+			id:          userID,
+			tokenString: tokenString2,
+			tokenSecret: tokenSecret,
+			wantErr:     true,
+		},
+		{
+			name:        "Wrong secret",
+			id:          userID,
+			tokenString: tokenString1,
+			tokenSecret: "NotMySecret",
+			wantErr:     true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tokenId, err := ValidateJWT(tt.tokenString, tt.tokenSecret)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ValidateJWT() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !tt.wantErr && tokenId != tt.id {
+				t.Errorf("Expected userID %v, got %v", tt.id, tokenId)
 			}
 		})
 	}
